9402109e7e8cd535ed5e47d0497f9be625880671
Lautaro-Reina <89100407+Lautaro-Reina@users.noreply.github.com>
2022-04-19 22:24:42 -0300

    Update index

--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
-/* VARIABLE REACT EN SCOPE, ESTA PRIMER LINE SE PUEDE NO ESCRIBIR TAMBIEN */
 import React from "react"  
-/* VARIABLE REACTDOM EN SCOPE */
 import ReactDOM from "react-dom"
 
 import App from './App'
 import './Styles/App.scss'
 
 
-/* PARA PONER EL COMPONENTE EN EL DOM. TOMA DOS PARAMETROS:
-EL COMPONENTE Y EL LUGAR DEL DOM DONDE SE QUIERE MOSTRAR EL COMPONENTE
-*/
+
 ReactDOM.render(<App/>, document.getElementById('root'))
